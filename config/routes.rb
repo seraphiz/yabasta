76189cb2a1936d993cc772664a5431bf35014ee3
Cristian <cristian.arthur@hotmail.com>
2019-03-11 16:45:59 -0300

    validacion de true o false en los post, agregar campos en mi  perfil, falta post aprobados y por aprobar

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'locations/find_address'
+  get 'locations/find_address', to: 'locations#find_address'
   get 'abouts/home'
-  get 'tags/index'
+  get 'tags/index', to: 'tags#index'
   devise_for :users, controllers: {
     sessions: 'users/sessions', registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
-  get 'users/profile'
+resources :users, only: :index
 
   resources :posts do
     get :home, on: :collection
@@ -15,8 +15,6 @@ Rails.application.routes.draw do
   end 
 
   resources :tags, only: [:index, :show]
-
-  get 'abouts/home'
   root 'posts#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,33 @@
 
-Tag.destroy_all
-# SocialGroup.destroy_all
-tag = ["Prefiero no decirlo", "Amenaza", "Acoso verbal", "Acoso físico", "Estupro", "Homofobia", "Racismo", "Atentado en contral el pudor", "Exploración sexual", "Stalking", "Transfobia", "Violencia doméstica", "Xenofobia", "Otros"]
+# Tag.destroy_all
+# # SocialGroup.destroy_all
+# tag = ["Prefiero no decirlo", "Amenaza", "Acoso verbal", "Acoso físico", "Estupro", "Homofobia", "Racismo", "Atentado en contral el pudor", "Exploración sexual", "Stalking", "Transfobia", "Violencia doméstica", "Xenofobia", "Otros"]
 
-tag.each do |tag|
-    Tag.create(
-        title: tag
-    )
-end 
-
-# social_group = ["Homosexuales", "Bisexuales", "Transexuales", "Extrangeros", "Indígenas", "Negros", "Personas en situacion de discapacidad", "Mujeres"]
-
-# social_group.each do |asignacion|
-#     SocialGroup.create(
-#         name: asignacion
+# tag.each do |tag|
+#     Tag.create(
+#         title: tag
 #     )
 # end 
 
-User.destroy_all
-20.times do |i|
-    User.create(
-    name: Faker::Name.name,
-    email: Faker::Internet.email,
-    password: "123456",
-    genre: ["Masculino", "Femenino", "Otro"].sample,
-    birthdate: Faker::Date.birthday(18, 65),
-    username: "Joao das Neves #{i+1}"
-    )
-end
+# # social_group = ["Homosexuales", "Bisexuales", "Transexuales", "Extrangeros", "Indígenas", "Negros", "Personas en situacion de discapacidad", "Mujeres"]
+
+# # social_group.each do |asignacion|
+# #     SocialGroup.create(
+# #         name: asignacion
+# #     )
+# # end 
+
+# User.destroy_all
+# 20.times do |i|
+#     User.create(
+#     name: Faker::Name.name,
+#     email: Faker::Internet.email,
+#     password: "123456",
+#     genre: ["Masculino", "Femenino", "Otro"].sample,
+#     birthdate: Faker::Date.birthday(18, 65),
+#     username: "Joao das Neves #{i+1}"
+#     )
+# end
 
 Post.destroy_all
 tags = Tag.ids
@@ -44,7 +44,8 @@ periodo = ["Prefiro no decirlo", "Mañana", "Tarde", "Noche", "Madrugada"]
         address: Faker::Address.street_name,
         name: Faker::Name.name,
         content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam odio mollitia tempora.",
-        anonymous: [true, false].sample
+        anonymous: [true, false].sample,
+        active: [true, false].sample
 
     )
 end 
